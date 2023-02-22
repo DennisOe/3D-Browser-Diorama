061083a103439f250d007c6e0120433cdba6e97b
Dennis Oe <93004899+DennisOe@users.noreply.github.com>
2023-02-22 21:31:37 +0100

    multi asset loader, new camera

--- src/diorama.js
+++ src/diorama.js
@@ -1,26 +1,38 @@
 export class RenderScene {
-    constructor(canvas) {
+    constructor(canvas, assets) {
         this.engine = new BABYLON.Engine(canvas, true);
+        this.canvas = canvas
+        this.assetList = assets
         this.scene = this.CreateScene();
-
+        // render loop
         this.engine.runRenderLoop(() => {
             this.scene.render();
         });
-
+        // window resize policy
         window.addEventListener("resize", () => {
             this.engine.resize();
         });
     }
 
     CreateScene() {
+        // Setup default render scene
         const scene = new BABYLON.Scene(this.engine);
         scene.clearColor = BABYLON.Color3.White();
-        const camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 0, 10), scene);
-        camera.setTarget(BABYLON.Vector3.Zero());
-        camera.attachControl();
+        // camera
+        const camera = new BABYLON.ArcRotateCamera("camera", Math.PI / 2, Math.PI / 2.5, 25, new BABYLON.Vector3.Zero(), scene);
+        camera.attachControl(this.canvas, false);
+        // light
         const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         light.intensity = 0.7;
-        BABYLON.SceneLoader.Append("../assets/", "suzanne.gltf", scene);
+        // assets
+        this.LoadAssets(this.assetList, scene);
         return scene;
+    }
+
+    LoadAssets(assets, scene) {
+        // Load assets from given array
+        for (let eachAsset of assets) {
+            BABYLON.SceneLoader.Append("../assets/", eachAsset, scene);
+        }
     }
 }
