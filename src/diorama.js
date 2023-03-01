563ca7eca754d361f462d647a1b622f91ee6f20b
Dennis Oe <93004899+DennisOe@users.noreply.github.com>
2023-03-01 22:13:10 +0100

    CreateCamera added

--- src/diorama.js
+++ src/diorama.js
@@ -1,9 +1,10 @@
 export class RenderScene {
     constructor(canvas, assetsArray) {
-        this.engine = new BABYLON.Engine(canvas, true);
         this.canvas = canvas;
-        this.assets = []
+        this.assets = [];
         this.assetsArray = assetsArray;
+        this.camera;
+        this.engine = new BABYLON.Engine(canvas, true);
         this.scene = this.CreateScene();
         // render loop
         this.engine.runRenderLoop(() => {
@@ -20,13 +21,7 @@ export class RenderScene {
         const scene = new BABYLON.Scene(this.engine);
         scene.clearColor = new BABYLON.Color3(0, 0.5, 0.8);
         // camera
-        const camera = new BABYLON.ArcRotateCamera("camera1",
-                                                   Math.PI / 2,
-                                                   Math.PI / 2.1,
-                                                   25,
-                                                   new BABYLON.Vector3(0, 3, 0),
-                                                   scene);
-        camera.attachControl(this.canvas, false);
+        this.camera = this.CreateCamera(scene);
         // light
         const light = new BABYLON.HemisphericLight("lightKey",
                                                    new BABYLON.Vector3(0, 1, 0),
@@ -37,11 +32,28 @@ export class RenderScene {
         return scene;
     }
 
+    CreateCamera(scene) {
+        const camera = new BABYLON.ArcRotateCamera("camera1",
+                                                   Math.PI / 2,
+                                                   Math.PI / 2.2,
+                                                   20,
+                                                   new BABYLON.Vector3(0, 3, 0),
+                                                   scene);
+        camera.attachControl(this.canvas, false);
+        camera.wheelPrecision = 50;
+        camera.panningSensibility = 0;
+        camera.lowerRadiusLimit = 5;
+        camera.upperRadiusLimit = 25;
+        camera.upperBetaLimit = Math.PI / 1.9;
+        camera.lowerBetaLimit = Math.PI / 10;
+        return camera;
+    }
+
     LoadAssets(assetsArray, scene) {
         // Load assets from given array
         assetsArray.forEach(async eachAsset => {
-            const { geometries } = await BABYLON.SceneLoader.AppendAsync("../assets/", eachAsset, scene);
-            geometries.forEach(node => {
+            const { meshes } = await BABYLON.SceneLoader.AppendAsync("../assets/", eachAsset, scene);
+            meshes.forEach(node => {
                 this.assets.push(node)
             });
         });
