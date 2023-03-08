50859fb27b3efe0885ee4891dcbe30f67a024aed
Dennis Oe <93004899+DennisOe@users.noreply.github.com>
2023-03-09 00:23:02 +0100

    apply modifire and export all meshes

--- src/blender.py
+++ src/blender.py
@@ -8,5 +8,12 @@ def export(exp_data: dict[str, str]) -> None:
         bpy.ops.export_scene.gltf(filepath=exp_data["path"],
                                   use_visible=True)
 
+
+bpy.ops.wm.save_mainfile()
+for each_object in bpy.data.objects:
+    if "MESH" in each_object.type:
+        bpy.context.view_layer.objects.active = each_object
+        [bpy.ops.object.modifier_apply(modifier=modifire.name) for modifire in each_object.modifiers]
+
 export(dict(type="gltf",
             path=bpy.data.filepath.split("blender")[0] + "diorama.glb"))
